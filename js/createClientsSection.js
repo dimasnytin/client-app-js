2f673a4f56b76178688f93c97c7b5cbe0f4ab523
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-06-28 15:57:49 +0200

    add class list

--- js/createClientsSection.js
+++ js/createClientsSection.js
@@ -12,36 +12,44 @@ export const createClientsSection = () => {
   const sortingDisplayCreate = document.createElement('td');
   const sortingDisplayEdit = document.createElement('td');
   const sortingDisplayContacts = document.createElement('td');
-  const sortingDisplayActions = document.createElement('');
+  const sortingDisplayActions = document.createElement('td');
   const sortingDisplaySpan = document.createElement('span');
-  const addUserBtm = document.createElement('button');
-  const addUserSvgBtm = document.createElement('span');
+  const addUserBtn = document.createElement('button');
+  const addUserSvgBtn = document.createElement('span');
   const tableWrapper = document.createElement('div');
   const clientsTable = document.createElement('table');
   const tbody = document.createElement('tbody');
   const createSpan = document.createElement('span');
-  const edditSpan = document.createElement('span');
+  const editSpan = document.createElement('span');
 
   // добавляем стили к html
   section.classList.add('clients');
   tableWrapper.classList.add('clients__wrapper');
-  h1.classList.add('clients_heading');
+  h1.classList.add('clients__heading');
   tbody.classList.add('clients__tbody');
-  sortingDisplay.classList.add('clients__display', 'display__info');
+  sortingDisplay.classList.add('clients__display', 'display-info');
   sortingDisplayId.classList.add('display-info__item', 'display-info__item--id', 'sort-up');
   sortingDisplayName.classList.add('display-info__item', 'display-info__item--name', 'sort-down');
-  sortingDisplayCreate.classList.add('display-info__item', 'display-info__item--create', 'sort-up');
-  sortingDisplayEdit.classList.add('display-info__item', 'display-info__item--change', 'sort-up');
-  sortingDisplayContacts.classList.add('display-info__item', 'display-info__item--contacts');
-  sortingDisplayActions.classList.add('display-info__item', 'display-info__item--actions');
-  sortingDisplaySpan.classList.add('display-info__sorting');
-  addUserBtm.classList.add('clients__btn', 'btn-reset');
-  addUserSvgBtm.classList.add('clients__svg');
+  sortingDisplayCreate.classList.add();
+  sortingDisplayEdit.classList.add();
+  sortingDisplayContacts.classList.add();
+  sortingDisplayActions.classList.add();
+  sortingDisplaySpan.classList.add();
+
+
+
+
+
+
+
+  
+  addUserBt.classList.add('clients__btn', 'btn-reset');
+  addUserSvgBtn.classList.add('clients__svg');
   container.classList.add('container');
   main.classList.add('main');
   clientsTable.classList.add('clients__table');
   createSpan.classList.add('create__span');
-  edditSpan.classList.add('change__span');
+  editSpan.classList.add('change__span');
 
   // наполняем html контентом
   h1.textContent = 'Клиенты';
