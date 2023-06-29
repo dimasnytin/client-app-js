f669e8f374deb0d92e0a51185f8ea873b4f6d3cc
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-06-29 16:04:12 +0200

    add js btn svg

--- js/createClientsSection.js
+++ js/createClientsSection.js
@@ -19,7 +19,7 @@ export const createClientsSection = () => {
   const sortingDisplayActions = document.createElement('td');
   const sortingDisplaySpan = document.createElement('span');
   const addUserBtn = document.createElement('button');
-  const addUserSvgBtn = document.createElement('span');
+  const addUserBtnSvg = document.createElement('span');
   const tableWrapper = document.createElement('div');
   const clientsTable = document.createElement('table');
   const tbody = document.createElement('tbody');
@@ -40,7 +40,7 @@ export const createClientsSection = () => {
   sortingDisplayActions.classList.add('display-info__item', 'display-info__item--actions');
   sortingDisplaySpan.classList.add('display-info__sorting');
   addUserBtn.classList.add('clients__btn', 'btn-reset');
-  // addUserBtnSvg.classList.add('clients__svg');
+  addUserBtnSvg.classList.add('clients__svg');
   container.classList.add('container');
   main.classList.add('main');
   clientsTable.classList.add('clients__table');
