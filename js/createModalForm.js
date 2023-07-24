c18699aac85f4ff893ff7f28dd27cb5db41607d8
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-07-24 16:28:56 +0200

    add modal js file there link youtube

---
+++ js/createModalForm.js
@@ -0,0 +1,20 @@
+export const createClientsForm = () => {
+    const modalTitle = document.createElement('h2');
+    const modalClose = document.createElement('button');
+    const form = document.createElement('form');
+    const inputName = document.createElement('input');
+    const labelName = document.createElement('label');
+    const inputSurname = document.createElement('input');
+    const labelSurname = document.createElement('label');
+    const inputLastname = document.createElement('input');
+    const labelLastname = document.createElement('label');
+    const requiredName = document.createElement('span');
+    const requiredSurname = document.createElement('span');
+    const addContactBtn = document.createElement('button');
+    const contactBtnSvgDefault = document.createElement('span');
+    const contactBtnSvgHover = document.createElement('span');
+    const saveBtn = document.createElement('button');
+    const cancelBtn = document.createElement('button');
+}
+
+// https://youtu.be/Xe3gaz_0qFQ?list=PLktakr6XMkP-FBr68wgI8xzMsXzE-ZqZF&t=559
