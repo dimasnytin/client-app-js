5e9eb3785549414639506b350063022745e7b19c
dimasnytin <60892736+dimasnytin@users.noreply.github.com>
2023-07-26 14:41:04 +0200

    add svg js and add button

--- js/createModalForm.js
+++ js/createModalForm.js
@@ -6,8 +6,8 @@ export const createClientsForm = () => {
     const labelName = document.createElement('label');
     const inputSurname = document.createElement('input');
     const labelSurname = document.createElement('label');
-    const inputLastname = document.createElement('input');
-    const labelLastname = document.createElement('label');
+    const inputLastName = document.createElement('input');
+    const labelLastName = document.createElement('label');
     const requiredName = document.createElement('span');
     const requiredSurname = document.createElement('span');
     const addContactBtn = document.createElement('button');
@@ -23,15 +23,15 @@ export const createClientsForm = () => {
 
 
     modalTitle.classList.add('modal__title');
-    modalClose.classList.add('modal__close');
+    modalClose.classList.add('modal__close', 'btn-reset');
     form.classList.add('modal__form');
 
     inputName.classList.add('modal__input');
     labelName.classList.add('modal__label');
     inputSurname.classList.add('modal__input');
     labelSurname.classList.add('modal__label');
-    inputLastname.classList.add('modal__input');
-    labelLastname.classList.add('modal__label');
+    inputLastName.classList.add('modal__input');
+    labelLastName.classList.add('modal__label');
 
     requiredName.classList.add('modal__label');
     requiredSurname.classList.add('modal__label');
@@ -39,18 +39,47 @@ export const createClientsForm = () => {
     addContactBtn.classList.add('modal__btn-contact', 'modal__btn-contact--active');
 
     contactBtnSvgDefault.classList.add('btn-contact__svg', 'btn-contact__svg--default', 'btn-contact__svg--active');
-    contactBtnSvgHover.classList.add('modal__');
+    contactBtnSvgHover.classList.add('btn-contact__svg', 'btn-contact__svg--hover');
 
     saveBtn.classList.add('modal__btn-save', 'btn-reset', 'site-btn');
     cancelBtn.classList.add('modal__btn-back','btn-reset');
 
-    contactsBlock.classList.add('modal__');
+    contactsBlock.classList.add('modal__contact');
 
     formFloatingName.classList.add('form__floating');
     formFloatingSurname.classList.add('form__floating');
     formFloatingLastName.classList.add('form__floating');
 
-    // https://youtu.be/Xe3gaz_0qFQ?list=PLktakr6XMkP-FBr68wgI8xzMsXzE-ZqZF&t=1155
+    inputName.type = 'text';
+    inputSurname.type = 'text';
+    inputLastName.type = 'text';
 
+    inputName.placeholder = 'Имя';
+    inputSurname.placeholder = 'Фамилия';
+    inputLastName.placeholder = 'Отчество';
+
+
+    // определяем атрибут
+    labelName.for = 'floatingName';
+    labelSurname.for = 'floatingSurname';
+    labelLastName.for = 'floatingLastName';
+
+    // добавляем ID
+    inputName.id = 'floatingName';
+    inputSurname.id = 'floatingName';
+    inputLastName.id = 'floatingName';
+
+    // добавляем контент
+    modalTitle.textContent = 'Новый клиент';
+    labelName.textContent = 'Имя';
+    labelSurname.textContent = 'Фамилия';
+    labelLastName.textContent = 'Отчество';
+
+    // кнопки
+    addContactBtn.textContent = 'Добавить контакт';
+    saveBtn.textContent = 'Сохранить';
+    cancelBtn.textContent = 'Отмена';
+    requiredName.textContent = '*';
+    requiredSurname.textContent = '*';
 }
 
